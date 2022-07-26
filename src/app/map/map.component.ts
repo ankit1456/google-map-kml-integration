a7e2dfa8e575d2f05e4df634b0d6de8d6cf3ef1b
ankittripathi <ankittripathi1456@gmail.com>
2022-07-26 10:43:53 +0530

    added some styling

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -8,21 +8,23 @@ declare const google: any;
   styleUrls: ['./map.component.scss'],
 })
 export class MapComponent implements OnInit, AfterViewInit {
-  url: string = 'https://googlearchive.github.io/js-v2-samples/ggeoxml/cta.kml';
+  url: string = '';
   map: any;
   @ViewChild('mapElement') mapElement: any;
   constructor() {}
 
   submit(f: NgForm) {
     this.url = f.controls['url'].value;
-    console.log(this.url);
+
+    
     this.ngAfterViewInit();
+    this.url=""
   }
 
   ngAfterViewInit(): void {
     const mapProperties = {
       center: new google.maps.LatLng(35.2271, -80.8431),
-      zoom: 15,
+      zoom: 5,
       mapTypeId: google.maps.MapTypeId.ROADMAP,
     };
     this.map = new google.maps.Map(
@@ -36,6 +38,8 @@ export class MapComponent implements OnInit, AfterViewInit {
       // url: 'https://heremaps.github.io/maps-api-for-javascript-examples/display-kml-on-map/data/us-states.kml',
       map: this.map,
     });
+
+    
   }
   ngOnInit(): void {}
 }
